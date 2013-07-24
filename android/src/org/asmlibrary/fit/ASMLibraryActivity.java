660384e8e572f115037c4cd97a4a44958429b61f
greatyao <njustyw@gmail.com>
2013-07-24 14:59:07 +0800

    Add Choice for two detector and MenuItem

--- android/src/org/asmlibrary/fit/ASMLibraryActivity.java
+++ android/src/org/asmlibrary/fit/ASMLibraryActivity.java
@@ -12,18 +12,15 @@ import org.opencv.android.OpenCVLoader;
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.highgui.Highgui;
-import org.opencv.core.MatOfRect;
-import org.opencv.core.Rect;
 import org.opencv.core.Scalar;
-import org.opencv.core.Size;
 import org.opencv.core.Point;
 import org.opencv.android.CameraBridgeViewBase;
 import org.opencv.android.CameraBridgeViewBase.CvCameraViewListener2;
-import org.opencv.objdetect.CascadeClassifier;
 import org.asmlibrary.fit.R;
 import org.asmlibrary.fit.ASMFit;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
@@ -35,24 +32,50 @@ public class ASMLibraryActivity extends Activity implements CvCameraViewListener
     
 	private static final String    TAG                 = "ASMLibraryDemo";
     
-    private Mat                    mRgba;
-    private Mat                    mGray;
-    private File                   mCascadeFile;
-    private File                   mModelFile;
-    private ASMFit      		   mASMFit;
-    private long				   mFrame;
+    private Mat                    	mRgba;
+    private Mat                    	mGray;
+    private File                   	mCascadeFile;
+    private File                   	mFastCascadeFile;
+    private File                   	mModelFile;
+    private ASMFit      		   	mASMFit;
+    private long				   	mFrame;
     private boolean					mFlag;
+    private boolean					mFastDetect = false;
     private Mat						mShape;
-    private static final Scalar mColor = new Scalar(255, 0, 0);
-    
-    
-    private CameraBridgeViewBase   mOpenCvCameraView;
+    private static final Scalar 	mColor = new Scalar(255, 0, 0);
+    private MenuItem               	mHelpItem;
+    private MenuItem               	mDetectItem;
+    private CameraBridgeViewBase   	mOpenCvCameraView;
     
     public ASMLibraryActivity() {
         Log.i(TAG, "Instantiated new " + this.getClass());
     }
     
     private BaseLoaderCallback  mLoaderCallback = new BaseLoaderCallback(this) {
+    	private File getSourceFile(int id, String name, String folder){
+    		File file = null;
+    		try {
+	    		InputStream is = getResources().openRawResource(id);
+	            File cascadeDir = getDir(folder, Context.MODE_PRIVATE);
+	            file = new File(cascadeDir, name);
+	            FileOutputStream os = new FileOutputStream(file);
+	            
+	            byte[] buffer = new byte[4096];
+	            int bytesRead;
+	            while ((bytesRead = is.read(buffer)) != -1) {
+	                os.write(buffer, 0, bytesRead);
+	            }
+	            is.close();
+	            os.close();
+    		}catch (IOException e) {
+                e.printStackTrace();
+                Log.e(TAG, "Failed to load file " + name + ". Exception thrown: " + e);
+            }
+	            
+            return file;
+    		
+    	}
+    	
         @Override
         public void onManagerConnected(int status) {
             switch (status) {
@@ -66,101 +89,48 @@ public class ASMLibraryActivity extends Activity implements CvCameraViewListener
                     
                     mASMFit = new ASMFit();
 
-                    try {
-                        // load model file from application resources
-                        InputStream is = getResources().openRawResource(R.raw.my68_1d);
-                        File cascadeDir = getDir("model", Context.MODE_PRIVATE);
-                        mModelFile = new File(cascadeDir, "my68_1d.amf");
-                        FileOutputStream os = new FileOutputStream(mModelFile);
+                    mModelFile = getSourceFile(R.raw.my68_1d, "my68_1d.amf", "model");
+                    if(mModelFile != null)
+                    	mASMFit.nativeReadModel(mModelFile.getAbsolutePath());
+                    
+                    
+                    mCascadeFile = getSourceFile(R.raw.haarcascade_frontalface_alt2, 
+                    		"haarcascade_frontalface_alt2.xml", "cascade");
+                    if(mCascadeFile != null)
+                    	mASMFit.nativeInitCascadeDetector(mCascadeFile.getAbsolutePath());
 
-                        byte[] buffer = new byte[4096];
-                        int bytesRead;
-                        while ((bytesRead = is.read(buffer)) != -1) {
-                            os.write(buffer, 0, bytesRead);
-                        }
-                        is.close();
-                        os.close();
-
-                        mASMFit.nativeReadModel(mModelFile.getAbsolutePath());
-
-                        cascadeDir.delete();
-
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                        Log.e(TAG, "Failed to load model. Exception thrown: " + e);
-                    }
-
-                    try {
-                        // load cascade file from application resources
-                        InputStream is = getResources().openRawResource(R.raw.haarcascade_frontalface_alt2);
-                        File cascadeDir = getDir("cascade", Context.MODE_PRIVATE);
-                        mCascadeFile = new File(cascadeDir, "haarcascade_frontalface_alt2.xml");
-                        FileOutputStream os = new FileOutputStream(mCascadeFile);
-
-                        byte[] buffer = new byte[4096];
-                        int bytesRead;
-                        while ((bytesRead = is.read(buffer)) != -1) {
-                            os.write(buffer, 0, bytesRead);
-                        }
-                        is.close();
-                        os.close();
-
-                        mASMFit.nativeInitCascadeDetector(mCascadeFile.getAbsolutePath());
-
-                        cascadeDir.delete();
-
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                        Log.e(TAG, "Failed to load cascade. Exception thrown: " + e);
-                    }
+                    mFastCascadeFile = getSourceFile(R.raw.lbpcascade_frontalface, 
+                    		"lbpcascade_frontalface.xml", "cascade");
+                    if(mFastCascadeFile != null)
+                    	mASMFit.nativeInitFastCascadeDetector(mFastCascadeFile.getAbsolutePath());
                     
                     //test image alignment
-                    try {
-                        // load image file from application resources
-                        InputStream is = getResources().openRawResource(R.raw.gump);
-                        File cascadeDir = getDir("image", Context.MODE_PRIVATE);
-                        File JPGFile = new File(cascadeDir, "gump.jpg");
-                        FileOutputStream os = new FileOutputStream(JPGFile);
-
-                        byte[] buffer = new byte[4096];
-                        int bytesRead;
-                        while ((bytesRead = is.read(buffer)) != -1) {
-                            os.write(buffer, 0, bytesRead);
-                        }
-                        is.close();
-                        os.close();
-
-                        Mat image = Highgui.imread(JPGFile.getAbsolutePath(), Highgui.IMREAD_UNCHANGED);
-                        Mat shapes = new Mat();
-                        
-                        if(mASMFit.detectAll(image, shapes) == true)
-            			{
-                        	/*
-                        	for(int i = 0; i < shapes.row(0).cols()/2; i++)
-            				{
-                            	Log.d(TAG, "before points:" + 
-                            			shapes.get(0, 2*i)[0] +"," +shapes.get(0, 2*i+1)[0]);
-            				}
-            				*/
-                        	
-            				mASMFit.fitting(image, shapes);
-            				
-            				/*
-            				for(int i = 0; i < shapes.row(0).cols()/2; i++)
-            				{
-                            	Log.d(TAG, "after points:" + 
-                            			shapes.get(0, 2*i)[0] +"," +shapes.get(0, 2*i+1)[0]);
-            				}
-            				*/
-            			}
-            				
-                        cascadeDir.delete();
-
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                        Log.e(TAG, "Failed to load cascade. Exception thrown: " + e);
-                    }
-
+                    // load image file from application resources
+                	File JPGFile = getSourceFile(R.raw.gump, "gump.jpg", "image");
+                	
+                	Mat image = Highgui.imread(JPGFile.getAbsolutePath(), Highgui.IMREAD_GRAYSCALE);
+                    Mat shapes = new Mat();
+                    
+                    if(mASMFit.detectAll(image, shapes) == true)
+        			{
+                    	/*
+                    	for(int i = 0; i < shapes.row(0).cols()/2; i++)
+        				{
+                        	Log.d(TAG, "before points:" + 
+                        			shapes.get(0, 2*i)[0] +"," +shapes.get(0, 2*i+1)[0]);
+        				}
+        				*/
+                    	
+        				mASMFit.fitting(image, shapes);
+        				
+        				/*
+        				for(int i = 0; i < shapes.row(0).cols()/2; i++)
+        				{
+                        	Log.d(TAG, "after points:" + 
+                        			shapes.get(0, 2*i)[0] +"," +shapes.get(0, 2*i+1)[0]);
+        				}
+        				*/
+        			}
 
                     mOpenCvCameraView.enableView();
                 } break;
@@ -205,7 +175,38 @@ public class ASMLibraryActivity extends Activity implements CvCameraViewListener
         mFrame = 0;
         mFlag = false;
     }
+    
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        Log.i(TAG, "called onCreateOptionsMenu");
+        if(mFastDetect == true)
+        	mDetectItem = menu.add("CascadeDetector");
+        else
+        	mDetectItem = menu.add("FastCascadeDetector");
+        mHelpItem = menu.add("About ASMLibrary");
+        return true;
+    }
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Log.i(TAG, "called onOptionsItemSelected; selected item: " + item);
+        if (item == mHelpItem)
+        	new AlertDialog.Builder(this).setTitle("About ASMLibrary")
+        		.setMessage("ASMLibrary -- A compact SDK for face alignment/tracking\n" +
+        				"Copyright (c) 2008-2011 by Yao Wei, all rights reserved.\n" +
+        				"Contact: njustyw@gmail.com\n")
+        				.setPositiveButton("OK", null).show();
+        else if(item == mDetectItem)
+        {
+        	if(mFastDetect == false)
+        		mFastDetect = true;
+        	else
+        		mFastDetect = false;
+        }
+        return true;
+    }
 
+    @Override
     public void onDestroy() {
         super.onDestroy();
         mOpenCvCameraView.disableView();
@@ -231,17 +232,22 @@ public class ASMLibraryActivity extends Activity implements CvCameraViewListener
 
         if(mFrame == 0 || mFlag == false)
 		{
-			mFlag = mASMFit.detectOne(mGray, mShape);
+        	Mat detShape = new Mat();
+			if(mFastDetect)
+				mFlag = mASMFit.fastDetectAll(mGray, detShape);
+			else
+				mFlag = mASMFit.detectAll(mGray, detShape);
+			if(mFlag)	mShape = detShape.row(0);
+		}
 			
-			if(mFlag) 
-			{
-				mFlag = mASMFit.videoFitting(mGray, mShape, mFrame);
-				
-				for(int i = 0; i < mShape.row(0).cols()/2; i++)
-				{ 
-					Point pt = new Point(mShape.get(0, 2*i)[0], mShape.get(0, 2*i+1)[0]);
-					Core.circle(mRgba, pt, 3, mColor);
-				}
+		if(mFlag) 
+		{
+			mFlag = mASMFit.videoFitting(mGray, mShape, mFrame);
+			
+			for(int i = 0; i < mShape.row(0).cols()/2; i++)
+			{ 
+				Point pt = new Point(mShape.get(0, 2*i)[0], mShape.get(0, 2*i+1)[0]);
+				Core.circle(mRgba, pt, 3, mColor);
 			}
 		}
 		
