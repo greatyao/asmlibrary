ecf9338149725070cce7c3c3e0f5575e4ab49271
greatyao <njustyw@gmail.com>
2013-07-24 14:58:07 +0800

    Add nativeFastCascadeDetector

--- android/src/org/asmlibrary/fit/ASMFit.java
+++ android/src/org/asmlibrary/fit/ASMFit.java
@@ -9,11 +9,34 @@ public class ASMFit {
 	
 	public ASMFit(){}
 	
+	/**
+	 * This function can only be used after nativeInitFastCascadeDetector()
+	 * @param imageGray original gray image 
+	 * @param faces all faces' feature points 
+	 * @return true if found faces, false otherwise
+	 */
+	public boolean fastDetectAll(Mat imageGray, Mat faces){
+		return nativeFastDetectAll(imageGray.getNativeObjAddr(), 
+				faces.getNativeObjAddr());
+	}
+	
+	/**
+	 * This function can only be used after nativeInitCascadeDetector()
+	 * @param imageGray original gray image 
+	 * @param face all faces' feature points 
+	 * @return true if found faces, false otherwise
+	 */
 	public boolean detectAll(Mat imageGray, Mat faces){
 		return nativeDetectAll(imageGray.getNativeObjAddr(), 
 				faces.getNativeObjAddr());
 	}
 	
+	/**
+	 * This function can only be used after nativeInitCascadeDetector()
+	 * @param imageGray original gray image 
+	 * @param faces only one face's feature points 
+	 * @return true if found faces, false otherwise
+	 */
 	public boolean detectOne(Mat imageGray, Mat face){
 		return nativeDetectOne(imageGray.getNativeObjAddr(), 
 				face.getNativeObjAddr());
@@ -30,12 +53,26 @@ public class ASMFit {
 	}
 	
 	public static native boolean nativeReadModel(String modelName);
-	
+
+	/**
+	 * @param cascadeName could be haarcascade_frontalface_alt2.xml 
+	 * @return
+	 */
 	public static native boolean nativeInitCascadeDetector(String cascadeName);
 	public static native void nativeDestroyCascadeDetector();
 	
+	/**
+	 * @param cascadeName could be lbpcascade_frontalface.xml 
+	 * @return
+	 */
+	public static native boolean nativeInitFastCascadeDetector(String cascadeName);
+	public static native void nativeDestroyFastCascadeDetector();
+	
+	public static native void nativeInitShape(long faces);
+	
 	private static native boolean nativeDetectAll(long inputImage, long faces);
 	private static native boolean nativeDetectOne(long inputImage, long face);
+	private static native boolean nativeFastDetectAll(long inputImage, long faces);
 	
 	private static native void nativeFitting(long inputImage, long shapes);
 	private static native boolean nativeVideoFitting(long inputImage, long shape, long frame);
